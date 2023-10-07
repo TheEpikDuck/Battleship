ff193a6332a875baeb82a21ff3789e617f39d8cb
TheEpikDuck <zhujul24@7hills.org>
2023-10-07 17:01:46 -0400

    starting to add more code

--- BattleShip.java
+++ BattleShip.java
@@ -1,5 +1,28 @@
 public class BattleShip {
-    int row = 10;
-    int col = 10;
-    char[][] board = new char[row][col];
+public static void main(String[] args) {
+    int[][] board = new int[10][10];
+    int[][] ships = new int[3][2];
+    int[] shoot = new int[2];
+    int attempts=0,
+        shotHit=0;
+
+    intBoard(board)
+    intShips(ships)
+
+    System.out.println();
+
+    do
+    {
+        showBoard(board);
+        shoot(shoot);
+        attempts++
+
+        if(hit(shoot,ships)){
+            hint(shoot,ships,attempts);
+            shotHit++;
+        }
+        else
+        hint(shoot,ships,attempts);
+        changeboard(shoot,ships,board);
+    }
 }
--- PillBox.java
+++
@@ -1,7 +0,0 @@
-public class PillBox {
-    int[] box;
-    int[] box = box[10];
-    int[] box = {1,2,3,4,5,6,7,8,9,11};
-
-    box[2][c] //row first column second6
-}
